3b4a82c7f47e17eed6e7863e5a6eb7521a7b3c00
nut <nut911mu13@gmail.com>
2017-12-09 11:01:09 +0700

    test

---
+++ NutPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+//This is Add Library
+//นี่คือตัวอย่างการเพิ่ม Library (command +/ )
+import UIKit
+
+//การประกาศตัวแปร แบบที่สามารถแก้ไขคำได้
+// นี่คือการประกาศตัวแปร แบบ String , Int,Double,Bool ที่ใช้คำในการกำหนด DataType
+var nameString = "NUT"
+var numberInt = 123
+var numberDouble = 12.34
+var statusBol = true
+
+//Try Change Value
+nameString = "Doramon"
+numberInt = 321
